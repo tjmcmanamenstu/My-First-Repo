ea7449c98a2e89c64bedc7cea5afc708fb7e507b
Thomas McManamen <tjmcmanamen@stu.naperville203.org>
2022-08-23 14:19:38 -0500

    BlueJ and Notes

---
+++ My First BlueJ Project/ch1Notes.java
@@ -0,0 +1,43 @@
+
+/**
+ * Thomas McManamen
+ * 8/23/2022
+ * Chapter 1 Notes
+ * Notes from class
+ * Who helped me: Mr. Kischuk, Mom, Dog
+ *
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class ch1Notes
+{
+    /**
+     * this is code notes for ch 1
+     *
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        // basic data types
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number2 = 9;
+        double number1 = 8.9, number3;
+        short num = 6;
+        long numbbbbbber = 9999;
+        
+        
+        //These are Objects
+        //Class  id      memory  Constructor
+        String   word =    new   String("Thomas McManamen");
+        Integer  number =  new   Integer(47);
+        Double   anumber = new   Double(45.8);
+        
+        
+        System.out.print(word);
+    }
+}
